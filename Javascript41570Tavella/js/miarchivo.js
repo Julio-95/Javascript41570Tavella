da257900db13c20bb49284d9cae7d6f99fbc2b0e
Julio-95 <106175572+Julio-95@users.noreply.github.com>
2022-08-07 14:47:43 -0300

    Add files via upload

---
+++ Javascript41570Tavella/js/miarchivo.js
@@ -0,0 +1,14 @@
+
+	let examen = parseInt(prompt("Nota del Examen"));
+	let final = parseInt(prompt("Notal del Examen Final"));
+	let promedio = (examen+final)/2;
+	
+	if(promedio > 10){
+		alert("Las notas introducidas deben tener valor entre 0 y 10 ")
+	}
+	else if(promedio >= 7){
+		alert("El promedio es "+promedio+" APROBADO");
+	}
+	else{
+		alert("El promedio es "+promedio+" DESAPROBADO");
+	}
