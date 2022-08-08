ed93e4066f9e46989cbdd94f780e9ca27f0effdb
Julio-95 <106175572+Julio-95@users.noreply.github.com>
2022-08-08 11:20:50 -0300

    Update miarchivo.js

--- Javascript41570Tavella/js/miarchivo.js
+++ Javascript41570Tavella/js/miarchivo.js
@@ -1,8 +1,10 @@
 
-	let examen = parseInt(prompt("Nota del Examen"));
+ let examen = parseInt(prompt("Nota del Examen"));
 	let final = parseInt(prompt("Notal del Examen Final"));
-	let promedio = (examen+final)/2;
-	
+	let promedio =  (examen+final)/2;
+	function calcularPromedio(){
+		return promedio
+	}
 	if(promedio > 10){
 		alert("Las notas introducidas deben tener valor entre 0 y 10 ")
 	}
